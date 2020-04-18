f8e9cc60e9684159c7ba40c2e6d41a9da0a1395b
anuragralla <anurag.r@ufl.edu>
2020-04-18 12:48:05 -0400

    First Commit

---
+++ app/src/main/java/com/example/mtcnn/OverlayView.java
@@ -0,0 +1,86 @@
+package com.example.mtcnn;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+
+public class OverlayView extends LinearLayout {
+    private Bitmap windowFrame;
+
+    public OverlayView(Context context) {
+        super(context);
+    }
+
+    public OverlayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public OverlayView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+
+        if (windowFrame == null) {
+            createWindowFrame();
+        }
+
+        canvas.drawBitmap(windowFrame, 0, 0, null);
+    }
+
+    protected void createWindowFrame() {
+
+        int w = getWidth();
+        int h = getHeight();
+
+        windowFrame = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas osCanvas = new Canvas(windowFrame);
+
+        RectF outerRectangle = new RectF(0, 0, getWidth(), getHeight());
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.parseColor("#000000"));
+        osCanvas.drawRect(outerRectangle, paint);
+
+        paint.setColor(Color.TRANSPARENT);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+
+        setPadding(80,50,80,50);
+
+        int pl = getPaddingLeft();
+        int pr = getPaddingRight();
+        int pt = getPaddingTop();
+        int pb = getPaddingBottom();
+
+        int usableWidth = w - (pl + pr);
+        int usableHeight = h - (pt + pb);
+
+        int radius = Math.min(usableWidth, usableHeight) / 2;
+        int cx = pl + (usableWidth / 2);
+        int cy = pt + (usableHeight / 2);
+
+        osCanvas.drawCircle(cx, cy, radius, paint);
+    }
+
+    @Override
+    public boolean isInEditMode() {
+        return true;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+
+        windowFrame = null;
+    }
+}
